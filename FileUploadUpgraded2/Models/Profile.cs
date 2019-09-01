4177371cfd7d782fcc18095b37f159f2e2c460b4
abdullah.demirtas <ceabdullahdemirtas@gmail.com>
2019-09-01 16:09:28 +0300

    Add project files.

---
+++ FileUploadUpgraded2/Models/Profile.cs
@@ -0,0 +1,8 @@
+﻿namespace FileUploadUpgraded2.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string AvatarPath { get; set; }
+    }
+}
---
+++ FileUploadUpgraded2/ViewModels/UploadedData.cs
@@ -0,0 +1,13 @@
+﻿namespace FileUploadUpgraded2.ViewModels
+{
+    public class UploadedData
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Zipcode { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
